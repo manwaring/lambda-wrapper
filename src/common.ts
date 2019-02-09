078477f4ca80a90274ff2a5895650e7bcc53d6df
Phillip <phillip.manwaring@gmail.com>
2019-02-09 00:36:19 -0500

    depend on parent having iopipe or epsagon

--- src/common.ts
+++ src/common.ts
@@ -1,40 +1,119 @@
-import { label } from 'epsagon';
-import { metric } from '@iopipe/iopipe';
+let epsagon: boolean, epsagon_label, iopipe: boolean, iopipe_metric;
+try {
+  const lib = require('epsagon');
+  epsagon = true;
+  epsagon_label = lib.label;
+} catch (err) {
+  console.log('Epsagon not installed in project, not using for tagging');
+}
+try {
+  const lib = require('@iopipe/iopipe');
+  iopipe = true;
+  iopipe_metric = lib.metric;
+} catch (err) {
+  console.log('IOPipe not installed in project, not using for labelling');
+}
 
 export function tagCommonMetrics(): void {
-  label('revision', process.env.REVISION);
-  label('stage', process.env.STAGE);
-  metric('region', process.env.AWS_REGION);
-  metric('revision', process.env.REVISION);
-  metric('stage', process.env.STAGE);
+  const { REVISION, STAGE, AWS_REGION } = process.env;
+  if (epsagon) {
+    label('revision', REVISION);
+    label('stage', STAGE);
+  }
+  if (iopipe) {
+    metric('region', AWS_REGION);
+    metric('revision', REVISION);
+    metric('stage', STAGE);
+  }
+  if (noLogLibraries()) {
+    console.log(`revision: ${REVISION}, stage: ${STAGE}, region: ${AWS_REGION}`);
+  }
 }
 
 export function tagSuccess(information?: any): void {
-  label('success');
-  metric('success', information);
+  if (epsagon) {
+    label('success');
+  }
+  if (iopipe) {
+    metric('success', information);
+  }
+  if (noLogLibraries()) {
+    console.log('success', information);
+  }
 }
 
 export function tagValid(information?: any): void {
-  label('valid');
-  metric('valid', information);
+  if (epsagon) {
+    label('valid');
+  }
+  if (iopipe) {
+    metric('valid', information);
+  }
+  if (noLogLibraries()) {
+    console.log('valid', information);
+  }
 }
 
 export function tagInvalid(information?: any): void {
-  label('invalid');
-  metric('invalid', information);
+  if (epsagon) {
+    label('invalid');
+  }
+  if (iopipe) {
+    metric('invalid', information);
+  }
+  if (noLogLibraries()) {
+    console.log('invalid', information);
+  }
 }
 
 export function tagRedirect(information?: any): void {
-  label('redirect');
-  metric('redirect', information);
+  if (epsagon) {
+    label('redirect');
+  }
+  if (iopipe) {
+    metric('redirect', information);
+  }
+  if (noLogLibraries()) {
+    console.log('redirect', information);
+  }
 }
 
 export function tagError(information?: any): void {
-  label('error');
-  metric('error', information);
+  if (epsagon) {
+    label('error');
+  }
+  if (iopipe) {
+    metric('error', information);
+  }
+  if (noLogLibraries()) {
+    console.log('error', information);
+  }
 }
 
 export function tagFailure(information?: any): void {
-  label('failure');
-  metric('failure', information);
+  if (epsagon) {
+    label('failure');
+  }
+  if (iopipe) {
+    metric('failure', information);
+  }
+  if (noLogLibraries()) {
+    console.log('failure', information);
+  }
+}
+
+function label(key: string, value: any = false) {
+  if (epsagon) {
+    value ? epsagon_label(key, value) : epsagon_label(key);
+  }
+}
+
+function metric(key: string, value: any = false) {
+  if (iopipe) {
+    value ? iopipe_metric(key, value) : iopipe_metric(key);
+  }
+}
+
+function noLogLibraries(): boolean {
+  return !epsagon && !iopipe;
 }
