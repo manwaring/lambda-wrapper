c033071056f2f635e6005c93ffa0e42bf02d0e7e
Phillip <phillip.manwaring@gmail.com>
2019-02-09 00:45:11 -0500

    update epsagon and iopipe tagging

--- src/common.ts
+++ src/common.ts
@@ -1,29 +1,30 @@
-let epsagon: boolean, epsagon_label, iopipe: boolean, iopipe_metric;
+let epsagon: boolean, epsagon_label, iopipe: boolean, iopipe_metric, iopipe_label;
 try {
   const lib = require('epsagon');
   epsagon = true;
   epsagon_label = lib.label;
 } catch (err) {
-  console.log('Epsagon not installed in project, not using for tagging');
+  console.log('Epsagon not installed in project, not tagging Epsagon labels');
 }
 try {
   const lib = require('@iopipe/iopipe');
   iopipe = true;
   iopipe_metric = lib.metric;
+  iopipe_label = lib.label;
 } catch (err) {
-  console.log('IOPipe not installed in project, not using for labelling');
+  console.log('IOPipe not installed in project, not tagging with IOPipe metrics');
 }
 
 export function tagCommonMetrics(): void {
   const { REVISION, STAGE, AWS_REGION } = process.env;
   if (epsagon) {
-    label('revision', REVISION);
-    label('stage', STAGE);
+    tagEpsagon('revision', REVISION);
+    tagEpsagon('stage', STAGE);
   }
   if (iopipe) {
-    metric('region', AWS_REGION);
-    metric('revision', REVISION);
-    metric('stage', STAGE);
+    tagIOPipe('region', AWS_REGION);
+    tagIOPipe('revision', REVISION);
+    tagIOPipe('stage', STAGE);
   }
   if (noLogLibraries()) {
     console.log(`revision: ${REVISION}, stage: ${STAGE}, region: ${AWS_REGION}`);
@@ -32,10 +33,10 @@ export function tagCommonMetrics(): void {
 
 export function tagSuccess(information?: any): void {
   if (epsagon) {
-    label('success');
+    tagEpsagon('success');
   }
   if (iopipe) {
-    metric('success', information);
+    tagIOPipe('success', information);
   }
   if (noLogLibraries()) {
     console.log('success', information);
@@ -44,10 +45,10 @@ export function tagSuccess(information?: any): void {
 
 export function tagValid(information?: any): void {
   if (epsagon) {
-    label('valid');
+    tagEpsagon('valid');
   }
   if (iopipe) {
-    metric('valid', information);
+    tagIOPipe('valid', information);
   }
   if (noLogLibraries()) {
     console.log('valid', information);
@@ -56,10 +57,10 @@ export function tagValid(information?: any): void {
 
 export function tagInvalid(information?: any): void {
   if (epsagon) {
-    label('invalid');
+    tagEpsagon('invalid');
   }
   if (iopipe) {
-    metric('invalid', information);
+    tagIOPipe('invalid', information);
   }
   if (noLogLibraries()) {
     console.log('invalid', information);
@@ -68,10 +69,10 @@ export function tagInvalid(information?: any): void {
 
 export function tagRedirect(information?: any): void {
   if (epsagon) {
-    label('redirect');
+    tagEpsagon('redirect');
   }
   if (iopipe) {
-    metric('redirect', information);
+    tagIOPipe('redirect', information);
   }
   if (noLogLibraries()) {
     console.log('redirect', information);
@@ -80,10 +81,10 @@ export function tagRedirect(information?: any): void {
 
 export function tagError(information?: any): void {
   if (epsagon) {
-    label('error');
+    tagEpsagon('error');
   }
   if (iopipe) {
-    metric('error', information);
+    tagIOPipe('error', information);
   }
   if (noLogLibraries()) {
     console.log('error', information);
@@ -92,25 +93,28 @@ export function tagError(information?: any): void {
 
 export function tagFailure(information?: any): void {
   if (epsagon) {
-    label('failure');
+    tagEpsagon('failure');
   }
   if (iopipe) {
-    metric('failure', information);
+    tagIOPipe('failure', information);
   }
   if (noLogLibraries()) {
     console.log('failure', information);
   }
 }
 
-function label(key: string, value: any = false) {
+function tagEpsagon(key: string, value: any = false) {
   if (epsagon) {
     value ? epsagon_label(key, value) : epsagon_label(key);
   }
 }
 
-function metric(key: string, value: any = false) {
+function tagIOPipe(key: string, value: any = false) {
   if (iopipe) {
-    value ? iopipe_metric(key, value) : iopipe_metric(key);
+    iopipe_label(key);
+    if (value) {
+      iopipe_metric(key, value);
+    }
   }
 }
 
