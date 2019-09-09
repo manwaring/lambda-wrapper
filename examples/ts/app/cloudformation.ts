3ce1363b433bf31c354d5e8b8d16ce889c79e576
Phillip Manwaring <phillip.manwaring@gmail.com>
2019-09-09 18:18:56 -0400

    update examples

--- examples/ts/app/cloudformation.ts
+++ examples/ts/app/cloudformation.ts
@@ -1,10 +1,23 @@
 import { cloudFormation } from '@manwaring/lambda-wrapper';
 import 'source-map-support/register';
+// import { send, SUCCESS } from './send';
+import { sendResponse } from './native-send';
 
-export const handler = cloudFormation(async ({ event, success, failure }) => {
+// export const handler = cloudFormation(async ({ event, success, failure }) => {
+//   try {
+//     success(event);
+//   } catch (err) {
+//     failure(err);
+//   }
+// });
+
+export const handler = (event, context, callback) => {
   try {
-    return success(event);
+    // send(event, context, SUCCESS);
+    sendResponse(event, context);
   } catch (err) {
-    return failure(err);
+    console.log('Error', err);
+    // send(event, context, SUCCESS);
+    sendResponse(event, context);
   }
-});
+};
