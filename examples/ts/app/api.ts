5dcd9d7842b598733f10a3a0bdb9eb55188633ea
Phillip Manwaring <phillip.manwaring@gmail.com>
2020-01-20 13:47:29 -0500

    update examples with default value in signature

--- examples/ts/app/api.ts
+++ examples/ts/app/api.ts
@@ -1,8 +1,10 @@
 import { api } from '@manwaring/lambda-wrapper';
 import 'source-map-support/register';
 
-export const handler = api(async ({ event, success, error }) => {
+export const handler = api(async ({ event, path = {}, success, error }) => {
   try {
+    const { query } = path;
+    console.log(query);
     const replacer = (key, value) => {
       switch (key) {
         case 'prop-to-ignore':
